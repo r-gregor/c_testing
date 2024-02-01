0a3729f2fe36913769ef6f92901e6b9e9c0ebe8d
r-gregor <gredelonghi@gmail.com>
2024-02-01 07:20:49 +0100

    updated ct_088_BrthReminder ... 20240201_0721_d

--- ct_088_BrthReminder-02_20221222/valgrind-test.sh
+++ ct_088_BrthReminder-02_20221222/valgrind-test.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
-executable="./bin/BrthReminder"
+executable="./bin/BrthReminder_c"
 /usr/bin/valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes -s ${executable}
 
