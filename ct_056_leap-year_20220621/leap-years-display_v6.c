bf67914f1f87158607f7cd9b65bbec2d18cbd4a5
r-gregor <gredelonghi@gmail.com>
2024-03-21 12:57:09 +0100

    updated ct_056_ leap years ... 20240321_1257_en

--- ct_056_leap-year_20220621/leap-years-display_v6.c
+++ ct_056_leap-year_20220621/leap-years-display_v6.c
@@ -1,26 +1,39 @@
+/*
+ * leap-years-display_v6.c
+ * 20240321_06_en
+ */
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
-#define YEARS_PER_LINE 22
+#include <time.h>
 
-/**
- * v5
- */
+#define YEARS_PER_LINE 22
+#define GREGORIAN_CAL_INTRO 1582 // v6
 
 bool is_leap_year(int);
 void display_leap_years(int, int);
 void print_columns(int *, int);      // v5: 'int *' is same as 'int []'
+void show_intro();
+
+time_t now;
+struct tm *now_ptr;
+
 
 /* main */
 int main(int argc, char **argv) {
 
-	display_leap_years(1, 2205);
+	now = time(NULL);
+	now_ptr = localtime(&now);
+
+	show_intro();
+	display_leap_years(1, 2600);
 
 	return 0;
 } // end main
 
 /* returns true if year is leap year */
 bool is_leap_year(int year) {
+	if (year < GREGORIAN_CAL_INTRO) return false; // v6
 	if (year % 4 != 0) return false;
 	else if (year % 100 != 0) return true;
 	else if (year % 400 != 0) return false;
@@ -32,7 +45,7 @@ bool is_leap_year(int year) {
  * of an array
  */
 void display_leap_years(int starty, int endy) {
-	int yearsnum = endy - starty;
+	int yearsnum = endy + 1 - starty;
 	int size = yearsnum / 4;
 	int leapyears[size];
 	int specialyears[size];
@@ -44,12 +57,18 @@ void display_leap_years(int starty, int endy) {
 		if (is_leap_year(curryear)) {
 			leapyears[lcount] = curryear;
 			lcount++;
-		} else if (curryear % 4 == 0 && !is_leap_year(curryear)) {
+		} else if (curryear > GREGORIAN_CAL_INTRO && curryear % 4 == 0 && !is_leap_year(curryear)) {
 			specialyears[scount] = curryear;
 			scount++;
 		}
 		curryear++;
 	}
+
+	int thisyear = now_ptr->tm_year + 1900;
+	printf("Current year: %d -- is", thisyear);
+	printf("%s leap year\n", is_leap_year(thisyear)? "" : " NOT");
+
+	printf("---\n");
 
 	printf("Leap years from %d to %d:\n", starty, endy);
 	print_columns(leapyears, lcount);
@@ -76,3 +95,22 @@ void print_columns(int *array, int count) {  // v5: int '*array' is same as 'int
 	printf("%5d\n", *(array + count - 1));   // v5: '*(array + count - 1)' is same as 'array[count - 1]'
 }
 
+
+void show_intro() {
+
+	char *intro = "Every fourth year (years which, divided by 4, leave no remainder) is a\n"
+	"leap year except those evenly divisible by 100 unless they're also evenly\n"
+	"divisible by 400. This rule has been in effect since 1582 when the Gregorian\n"
+	"calendar was first introduced.\n"
+	"\n"
+	"    February 2000          February 2100          February 2400\n"
+	"Su Mo Tu We Th Fr Sa   Su Mo Tu We Th Fr Sa   Su Mo Tu We Th Fr Sa\n"
+	"       1  2  3  4  5       1  2  3  4  5  6          1  2  3  4  5\n"
+	" 6  7  8  9 10 11 12    7  8  9 10 11 12 13    6  7  8  9 10 11 12\n"
+	"13 14 15 16 17 18 19   14 15 16 17 18 19 20   13 14 15 16 17 18 19\n"
+	"20 21 22 23 24 25 26   21 22 23 24 25 26 27   20 21 22 23 24 25 26\n"
+	"27 28 29               28                     27 28 29\n"
+	"---\n";
+	
+	printf("%s", intro);
+}
--- ct_056_leap-year_20220621/src/leap-years-write-to-file_v3.c
+++ ct_056_leap-year_20220621/src/leap-years-write-to-file_v3.c
@@ -1,3 +1,7 @@
+/*
+ * leap-years-write-to-file_v3.c
+ * 20240321_03_en
+ */
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
@@ -5,21 +9,18 @@
 #include <time.h>
 
 #define YEARS_PER_LINE 22
-
-/**
- * v2
- * write to file: leap-years_YYYYmmdd_HHMMSS.txt
- */
-
+#define GREGORIAN_CAL_INTRO 1582 // v3
 
 bool is_leap_year(int);
 void write_leap_years(int, int);
 void write_columns(int *, int);
+
 FILE *fd;
 char *fname;
 
 time_t now;
 struct tm *now_ptr;
+
 
 /* main */
 int main(int argc, char **argv) {
@@ -27,8 +28,8 @@ int main(int argc, char **argv) {
 	now = time(NULL);
 	now_ptr = localtime(&now);
 
-	char fname[31];
-	sprintf(fname, "leap-years_%d%02d%02d_%02d%02d%02d.txt",
+	char fname[38];
+	sprintf(fname, "OUTPUT_leap-years_%d%02d%02d_%02d%02d%02d.txt",
 							now_ptr->tm_year + 1900,
 							now_ptr->tm_mon + 1,
 							now_ptr->tm_mday,
@@ -43,13 +44,15 @@ int main(int argc, char **argv) {
 		exit(EXIT_FAILURE);
 	}
 
-	write_leap_years(1, 2205);
+	write_leap_years(1, 2600);
+	printf("Output written to: %s\n", fname);
 
 	return 0;
 } // end main
 
 /* returns true if year is leap year */
 bool is_leap_year(int year) {
+	if (year < GREGORIAN_CAL_INTRO) return false; // v3
 	if (year % 4 != 0) return false;
 	else if (year % 100 != 0) return true;
 	else if (year % 400 != 0) return false;
@@ -61,7 +64,22 @@ bool is_leap_year(int year) {
  * of an array into an external file
  */
 void write_leap_years(int starty, int endy) {
-	int yearsnum = endy - starty;
+
+char *intro = "Every fourth year (years which, divided by 4, leave no remainder) is a\n"
+"leap year except those evenly divisible by 100 unless they're also evenly\n"
+"divisible by 400. This rule has been in effect since 1582 when the Gregorian\n"
+"calendar was first introduced.\n"
+"\n"
+"    February 2000          February 2100          February 2400\n"
+"Su Mo Tu We Th Fr Sa   Su Mo Tu We Th Fr Sa   Su Mo Tu We Th Fr Sa\n"
+"       1  2  3  4  5       1  2  3  4  5  6          1  2  3  4  5\n"
+" 6  7  8  9 10 11 12    7  8  9 10 11 12 13    6  7  8  9 10 11 12\n"
+"13 14 15 16 17 18 19   14 15 16 17 18 19 20   13 14 15 16 17 18 19\n"
+"20 21 22 23 24 25 26   21 22 23 24 25 26 27   20 21 22 23 24 25 26\n"
+"27 28 29               28                     27 28 29\n"
+"---\n";
+
+	int yearsnum = endy + 1 - starty;
 	int size = yearsnum / 4;
 	int leapyears[size];
 	int specialyears[size];
@@ -73,12 +91,20 @@ void write_leap_years(int starty, int endy) {
 		if (is_leap_year(curryear)) {
 			leapyears[lcount] = curryear;
 			lcount++;
-		} else if (curryear % 4 == 0 && !is_leap_year(curryear)) {
+		} else if (curryear > GREGORIAN_CAL_INTRO && curryear % 4 == 0 && !is_leap_year(curryear)) {
 			specialyears[scount] = curryear;
 			scount++;
 		}
 		curryear++;
 	}
+
+	fprintf(fd, "%s", intro);
+
+	int thisyear = now_ptr->tm_year + 1900;
+	fprintf(fd, "Current year: %d -- is", thisyear);
+	fprintf(fd, "%s leap year\n", is_leap_year(thisyear)? "" : " NOT");
+
+	fprintf(fd, "---\n");
 
 	fprintf(fd, "Leap years from %d to %d:\n", starty, endy);
 	write_columns(leapyears, lcount);
