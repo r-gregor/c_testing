bf8de9c316e6c39720f2bf669135ed76539f71c0
r-gregor <gredelonghi@gmail.com>
2024-06-04 10:44:50 +0200

    update 20240604_1444_en

---
+++ ct_OSTALO/console_size_01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <sys/ioctl.h>
+
+int main (void) {
+    struct winsize max;
+    ioctl(0, TIOCGWINSZ , &max);
+    printf ("lines %d\n", max.ws_row);
+    printf ("columns %d\n", max.ws_col);
+	
+	return 0;
+}
