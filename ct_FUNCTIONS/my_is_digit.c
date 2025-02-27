34a518159da0be78a69b1f4013a10f39daae5394
r-gregor <gredelonghi@gmail.com>
2025-02-27 16:00:46 +0100

    update 20250227_1600_en

--- ct_FUNCTIONS/my_is_digit.c
+++ ct_FUNCTIONS/my_is_digit.c
@@ -1,6 +1,6 @@
-bool my_is_digit(char *c);
+int my_is_digit(char *c);
 
-bool my_is_digit(char *c) {
+int my_is_digit(char *c) {
 	return ('0' <= *c <= '9');
 }
 
---
+++ ct_FUNCTIONS/my_strtol.c
@@ -0,0 +1,17 @@
+int my_is_digit(char *c);
+unsigned long long my_strtol(char *c);
+
+/* string to long */
+unsigned long long my_strtol(char *c) {
+	unsigned long long n = 0;
+	while (my_is_digit(*c)) {
+		n = n*10 + *c - '0';
+		c +=1;
+	}
+	return n;
+}
+
+int my_is_digit(char *c) {
+	return ('0' <= *c <= '9');
+}
+
