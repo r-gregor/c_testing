f226e513b7082b914104c61002e8be8d3c9bec63
r-gregor <gredelonghi@gmail.com>
2025-02-27 15:18:22 +0100

    update 20250227_1518_en

---
+++ ct_FUNCTIONS/my_strlen.c
@@ -0,0 +1,14 @@
+/*
+ * friom: Coding in C as My Mouse Dies ...
+ *        https://www.youtube.com/watch?v=UhJC0wsxc_I&t=4s
+ *        at: 01:50:57/02:59:22
+ */
+
+size_t my_strlen(const char *s);
+
+size_t my_strlen(const char *s) {
+	size_t n = 0;
+	while(*s++) n++;
+	return n;
+}
+
