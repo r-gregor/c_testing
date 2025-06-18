69756bd56d842ded4b9665611bc996194e4db2ac
r-gregor <gredelonghi@gmail.com>
2025-06-18 15:35:52 +0200

    update 20250618_1535_en

---
+++ ct_142_lookup_table_test_20250618/lookup-table-test.c
@@ -0,0 +1,48 @@
+/*
+ * filename: lookup-table-test.c
+ * 20250618 v1 en
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+const char *week_days[] = {
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Sunday"
+};
+
+void print_weekday(int num);
+
+int main(int argc, char **argv) {
+
+	print_weekday(1);
+	print_weekday(11);
+	print_weekday(14);
+	print_weekday(0);
+	print_weekday(-1);
+	print_weekday(24);
+	print_weekday(7);
+	print_weekday(9);
+	print_weekday(4);
+	print_weekday(3);
+	print_weekday(2);
+
+	return 0;
+}
+
+void print_weekday(int num) {
+	if (num < 0) {
+		// printf("Day of the week at number %5d: ", num);
+		// printf("-- must be 0 or greater\n");
+		return;
+	}
+
+	unsigned int day = num % 7;
+	printf("Day of the week at number %5d: is %s\n", num, week_days[day]);
+}
+
