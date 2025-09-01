773326981dd4ccb6e11556e850bee501ff68dcaf
r-gregor <gredelonghi@gmail.com>
2025-09-01 10:27:44 +0200

    update 20250901_1027_en

---
+++ ct_144-os-prober-20250901/os-prober.c
@@ -0,0 +1,34 @@
+/*
+ * os-prober.c
+ * last change: 20250901
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/utsname.h>
+
+#if defined(__linux__) // any linux distribution
+#define PLATFORM "LINUX"
+#elif defined(__unix__)
+#define PLATFORM "UNIX (other Linux)"
+#elif defined(_WIN32)
+#define PLATFORM "WINDOWS"
+#else
+#define PLATFORM "UNKNOWN"
+#endif
+
+
+int main(int argc, char *argv[]) {
+
+	int plcs = 15;
+	// puts(PLATFORM);
+	printf("[INFO] %-*s %-*s %s\n", plcs, "platform:", plcs +10, PLATFORM, "(defined platform)");
+	printf("[INFO] %-*s %-*s %s\n", plcs, "OS:", plcs + 10, getenv("OS"), "(\"OS\" env)");
+
+	struct utsname sysinfo;
+    if (uname(&sysinfo) != 0) {
+		printf("[ERROR] could note get sysinfo\n");
+	}
+	printf("[INFO] %-*s %-*s %s\n", plcs, "sysname:", plcs +10, sysinfo.sysname, "(utsname/sysname)");
+    
+	return 0;
+}
