17da7d8906798a7a8cb27d0f1e2d3d0259a59f4d
r-gregor <gredelonghi@gmail.com>
2025-09-23 15:28:02 +0200

    update 20250923_1528_en

---
+++ ct_147_compound_literals_20250923/c-compound-literals-test.c
@@ -0,0 +1,50 @@
+/*
+ * filename: c-compound-literals-test.c
+ * 20250923 v1 en
+ * last: 20250923
+ */
+#include <stdio.h>
+#include <string.h>
+
+#define START 48
+#define END  128
+
+typedef struct point {
+	int x;
+	int y;
+} point_t;
+
+void print_point_ptr(point_t *);
+void print_point(point_t);
+
+int main(int argc, char **argv) {
+
+	/* compound literal test 1: */
+	for (int i = START; i < END; ++i) {
+		printf("character at %3d is:            %c\n", i, i);
+		printf("character at %3d as string is: \"%s\"\n", i, (char[]) {i, '\0'});
+		//                                                   +________________+
+		//                                                    compound literal
+	}
+
+	/* compound literal test 2: */
+	printf("\n---\nMy name: '%s'\n", (char[]){71, 114, 101, 103, 111, 114, 32, 82, 101, 100, 101, 108, 111, 110, 103, 104, 105, '\0'});
+	//                                      G   r    e    g    o    r    _   R    e   d    e    l    o    n    g    h    i
+
+	printf("---\n");
+
+	/* compound literal test 3: */
+	print_point_ptr(&(point_t){.x = 2, .y = 7});
+	print_point((point_t){.x = 5, .y = -4});
+
+	return 0;
+}
+
+void print_point_ptr(point_t *p) {
+	printf("point: (%d, %d)\n", p->x, p->y);
+}
+
+void print_point(point_t p) {
+	printf("point: (%d, %d)\n", p.x, p.y);
+}
+
