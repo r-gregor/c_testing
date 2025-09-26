c98645c9917789a8e9d65cce62621b25efd23151
r-gregor <gredelonghi@gmail.com>
2025-09-26 09:27:22 +0200

    update 20250926_0927_en

---
+++ ct_148_generic-stack-test-20250924/04-multi-type-objects-multiple-modules/v3/mt_objects.h
@@ -0,0 +1,213 @@
+/*
+ * 20250926 v2 en
+ * 20250926 v3 en
+ */
+
+#ifndef _MT_OBJECTS_H
+#define _MT_OBJECTS_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/* structures */
+
+/* forward declared struct
+ * it will be used in circular dependency between:
+ * object_t -> object_data_t -> vector_t
+ */
+
+/* must be declared before object_data_t union whwere it is used -- size must be known! */
+typedef struct Object object_t;
+
+typedef struct Vector {
+	object_t *x;
+	object_t *y;
+	object_t *z;
+} vector_t;
+
+typedef struct Array {
+	size_t size;
+	object_t **elements;
+} array_t;
+
+typedef enum Object_kind {
+	INTEGER,
+	FLOAT,
+	STRING,
+	VECTOR3,
+	ARRAY
+} object_kind_t;
+
+typedef union Object_data {
+	int          v_int;
+	float      v_float;
+	char     *v_string;
+	vector_t v_vector3;
+	array_t    v_array;
+} object_data_t;
+
+typedef struct Object {
+	object_kind_t kind;
+	object_data_t data;
+} object_t;
+
+/* function declarations */
+object_t *new_integer(int value);
+object_t *new_float(int value);
+object_t *new_string(char *value);
+object_t *new_vector3(object_t *x, object_t *y, object_t *z);
+object_t *new_array(size_t size);
+bool array_set(object_t *obj, size_t index, object_t *value);
+object_t *array_get(object_t *obj, size_t index);
+int obj_get_len(object_t *obj);
+
+/* v2 */
+void obj_free_string(object_t *string);
+void obj_free_vesctor3(object_t *vector3);
+void obj_free_array(object_t *array);
+
+/* =============== implementation ================ */
+#define MT_OBJECT_IMPLEMENTATION
+
+#ifdef MT_OBJECT_IMPLEMENTATION
+
+/* function new_integer */
+object_t *new_integer(int value) {
+	object_t *obj = malloc(sizeof(object_t));
+	if (obj == NULL) { return NULL; }
+	
+	obj->kind = INTEGER;
+	obj->data.v_int = value;
+
+	return obj;
+}
+
+/* function new_float */
+object_t *new_float(int value) {
+	object_t *obj = malloc(sizeof(object_t));
+	if (obj == NULL) { return NULL; }
+	
+	obj->kind = FLOAT;
+	obj->data.v_float = value;
+
+	return obj;
+}
+
+/* function new_string */
+object_t *new_string(char *value) {
+	object_t *obj = malloc(sizeof(object_t));
+	if (obj == NULL) { return NULL; }
+	
+	obj->kind = STRING;
+	obj->data.v_string = malloc(strlen(value) + 1);
+	if (obj->data.v_string == NULL) { return NULL; }
+
+	strcpy(obj->data.v_string, value);
+
+	return obj;
+}
+
+/* function new_vector3
+ * --------------------
+ * object than can hold reference to another object
+ * it is a collection type that holds other types
+ */
+object_t *new_vector3(object_t *x, object_t *y, object_t *z) {
+	if (x == NULL || y == NULL || z == NULL) { return NULL; }
+
+	object_t *obj = malloc(sizeof(object_t));
+	if (obj == NULL) { return NULL; }
+	
+	obj->kind = VECTOR3;
+	obj->data.v_vector3.x = x;
+	obj->data.v_vector3.y = y;
+	obj->data.v_vector3.z = z;
+	return obj;
+}
+
+/* function new_array */
+object_t *new_array(size_t size) {
+	object_t *obj = malloc(sizeof(object_t));
+	if (obj == NULL) { return NULL; }
+	
+	obj->kind = ARRAY;
+
+	object_t **elements = calloc(size, sizeof(object_t *));
+	if (elements == NULL) { return NULL; }
+	array_t array = { .size = size, .elements = elements };
+
+	obj->data.v_array = array;
+
+	return obj;
+}
+
+/* function array_set */
+bool array_set(object_t *obj, size_t index, object_t *value) {
+	if (obj == NULL || value == NULL) { return false; }
+	if (obj->kind != ARRAY) { return false; }
+	if (obj->data.v_array.size <= index) { return false; }
+
+	obj->data.v_array.elements[index] = value;
+	return true;
+}
+
+/* function array_get */
+object_t *array_get(object_t *obj, size_t index) {
+	if (obj == NULL ) { return false; }
+	if (obj->kind != ARRAY) { return false; }
+	if (obj->data.v_array.size <= index) { return false; }
+
+	return obj->data.v_array.elements[index];
+}
+
+/* function obj_len */
+int obj_get_len(object_t *obj) {
+	if (obj == NULL) { return -1; }
+
+	switch (obj->kind) {
+		case INTEGER: return 1;
+		case FLOAT: return 1;
+		case STRING: return strlen(obj->data.v_string);
+		case VECTOR3: return 3;
+		case ARRAY: return obj->data.v_array.size;
+		default: return -1;
+	}
+}
+
+/* v2 */
+/* function obj_free_string */
+void obj_free_string(object_t *string) {
+	if (string == NULL) { return; }
+	free(string->data.v_string);
+	free(string);
+}
+
+/* function obj_free_vesctor3 */
+void obj_free_vesctor3 (object_t *vector3) {
+	if (vector3 == NULL) { return; }
+	vector_t coord = vector3->data.v_vector3;
+	free(coord.x);
+	free(coord.y);
+	free(coord.z);
+
+	free(vector3);
+}
+
+/* function obj_free_array */
+void obj_free_array(object_t *array) {
+	if (array == NULL) { return; }
+	size_t fmem_num = array->data.v_array.size;
+	for (int i = 0; i < fmem_num; i++) {
+		free(array->data.v_array.elements[i]->data.v_string);
+		free(array->data.v_array.elements[i]);
+	}
+	free(array->data.v_array.elements);
+	free(array);
+}
+
+#endif
+/* =============== implementation ================ */
+
+#endif
---
+++ ct_148_generic-stack-test-20250924/04-multi-type-objects-multiple-modules/v3/multi-type-objects-modules-test.c
@@ -0,0 +1,87 @@
+/*
+ * filename: multi-type-objects-modules-test-v1.c
+ * from: C Programming and Memory Management - Full Course --> Objects
+ *       https://www.youtube.com/watch?v=rJrd2QMVbGM
+ * at:   03:11:00 / 04:43:47
+ * ---
+ * 20250925 v1 d
+ * 20250926 v2 en
+ * last: 20250926
+ * STOPED on 20250924 at 03:35:30
+ */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "mt_objects.h"
+
+/* main */
+int main(int argc, char **argv) {
+	// printf("\n\t*** EVERYTHING SEEMS TO BE OK ***\n\n");
+
+	printf("---\n");
+	object_t *str1 = new_string("Gregor Redelonghi");
+	printf("string object value:   %s\n", str1->data.v_string);
+	/* v2
+	free(str1->data.v_string);
+	free(str1);
+	*/
+	obj_free_string(str1);
+
+	object_t *fn1 = new_float(134);
+	printf("float object value:    %f\n", fn1->data.v_float);
+	free(fn1);
+
+	object_t *int1 = new_integer(134);
+	printf("integer object value:  %d\n", int1->data.v_int);
+	free(int1);
+
+	object_t *vec31 = new_vector3(
+			new_integer(1),
+			new_integer(4),
+			new_integer(9));
+
+	vector_t coord = vec31->data.v_vector3;
+	printf("vector3 object value:  (%d, %d, %d)\n",
+			coord.x->data.v_int,
+			coord.y->data.v_int,
+			coord.z->data.v_int);
+
+	/* v2
+	free(coord.x);
+	free(coord.y);
+	free(coord.z);
+	free(vec31);
+	*/
+	obj_free_vesctor3(vec31);
+
+	printf("---\n");
+	size_t fmem_num = 6;
+	object_t *family1 = new_array(fmem_num);
+	if (! array_set(family1, 0, new_string("Gregor Redelonghi")))      printf("[ERROR]\n");
+	if (! array_set(family1, 1, new_string("Tadeja Mali Redelonghi"))) printf("[ERROR]\n");
+	if (! array_set(family1, 2, new_string("Zala Redelonghi")))        printf("[ERROR]\n");
+	if (! array_set(family1, 3, new_string("Mark Redelonghi")))        printf("[ERROR]\n");
+	if (! array_set(family1, 4, new_string("Špela Redelonghi")))       printf("[ERROR]\n");
+	if (! array_set(family1, 5, new_string("Sisi Redelonghi")))        printf("[ERROR]\n");
+
+	for (int i = 0; i < fmem_num; i++) {
+		printf("Family member %d: %s\n", i + 1, family1->data.v_array.elements[i]->data.v_string);
+	}
+
+	/* v2
+	for (int i = 0; i < fmem_num; i++) {
+		free(family1->data.v_array.elements[i]->data.v_string);
+		free(family1->data.v_array.elements[i]);
+	}
+	free(family1->data.v_array.elements);
+	free(family1);
+	*/
+	obj_free_array(family1);
+
+	printf("\n");
+
+	return 0;
+} /* END main */
+
