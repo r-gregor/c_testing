6bfd04145e4b9efce84af2e70ef65ade3b126c5d
r-gregor <gredelonghi@gmail.com>
2025-12-02 12:06:12 +0100

    update 20251202_1206_en

--- ct_149_rsort-by-tmstmp-absp_20251201/rsort-by-tmstmp-absp-c.c
+++ ct_149_rsort-by-tmstmp-absp_20251201/rsort-by-tmstmp-absp-c.c
@@ -3,7 +3,7 @@
  * v2 20251201  d: added abs path to fname to be used with fzf
  *                 correct valgrind test error: abs_path as char *pointer
  * v3 20251202 en: move everything from main into new function rsort_by_tmpstmp_abs
- * based on:rsort-by-tmstmp_c.c
+ * based on:rsort-by-tmstmp-c.c
  * last: 20251202
  */
 
