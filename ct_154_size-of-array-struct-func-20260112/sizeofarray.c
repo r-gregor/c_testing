3c91a65328b01d8e5e7d6710da870d212bbc5d17
r-gregor <gredelonghi@gmail.com>
2026-01-12 15:49:05 +0100

    update 20260112_1549_en

---
+++ ct_154_size-of-array-struct-func-20260112/sizeofarray.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct arristrc arristrc_t;
+
+size_t get_arr_elnum(int *arr, size_t arr_size);
+arristrc_t new_arr(int *arr, size_t arr_size);
+int get_nth_element(int *arr, int n);
+int get_nth_element_str(arristrc_t *arristri, size_t n);
+
+struct arristrc {
+	int *arr;
+	size_t count;
+};
+
+int main(int argc, char **argv) {
+
+	int nums1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	int nums2[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300};
+
+	/* by hand: BAD */
+	arristrc_t arr1;
+	arr1.arr = nums1;
+	arr1.count = get_arr_elnum(nums1, sizeof(nums1));
+
+	/* by func: GOOD */
+	arristrc_t arr2 = new_arr(nums2, sizeof(nums2));
+
+	/* TODO: display n-th element function */
+	printf("arr1 has %ld elements\n", arr1.count);
+	printf("arr2 has %ld elements\n", arr2.count);
+
+	printf("3-th element of arr1: %d\n", get_nth_element(arr1.arr, 3));
+	printf("11-th element of arr2: %d\n", get_nth_element(arr2.arr, 11));
+
+	printf("11-th element of arr2: %d\n", get_nth_element_str(&arr2, 11));
+	printf("11-th element of arr2: %d\n", get_nth_element_str(&arr2, 14));
+
+	return 0;
+}
+
+size_t get_arr_elnum(int *arr, size_t arr_size) {
+	return arr_size/sizeof(arr[0]);
+}
+
+arristrc_t new_arr(int *arr, size_t arr_size) {
+	arristrc_t newarr;
+	newarr.arr = arr;
+	newarr.count = arr_size/sizeof(arr[0]);
+	return newarr;
+}
+
+
+int get_nth_element(int *arr, int n) {
+	if (n <= 0) {
+		printf("Index must be al least 1!");
+		return -1;
+	}
+	return arr[n - 1];
+}
+
+int get_nth_element_str(arristrc_t *arristr, size_t n) {
+	if (n <= 0 || n >= arristr->count) {
+		printf("Index must be at least 1 and smaller as array size!");
+		return -1;
+	}
+	return arristr->arr[n - 1];
+}
+
