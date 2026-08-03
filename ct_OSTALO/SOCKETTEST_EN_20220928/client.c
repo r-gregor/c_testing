81a2f4b10382fc6910eb94b908660296c2168980
r-gregor <gredelonghi@gmail.com>
2026-08-03 13:40:08 +0200

    update 20260803_1340_en

--- ct_OSTALO/SOCKETTEST_EN_20220928/client.c
+++ ct_OSTALO/SOCKETTEST_EN_20220928/client.c
@@ -1,9 +1,9 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<sys/socket.h>
-#include<arpa/inet.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
 // Create a Socket for server communication
 short SocketCreate(void) {
--- ct_OSTALO/SOCKETTEST_EN_20220928/server.c
+++ ct_OSTALO/SOCKETTEST_EN_20220928/server.c
@@ -1,8 +1,8 @@
-#include<stdio.h>
-#include<string.h>
-#include<sys/socket.h>
-#include<arpa/inet.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
 short SocketCreate(void) {
 	short hSocket;
